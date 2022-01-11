d6a6c3cb5157a9e62c36894785c09b7ffae9d095
AndyTiTi <458666482@qq.com>
2022-01-11 15:20:19 +0800

    test travis ci

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ app.use(express.static(path.join(__dirname, '../dist')));
 
 app.listen(3000, err => {
   if (!err) {
-    console.log('监听成功，端口号3000');
+    console.log('监听成功，端口号3000！');
   } else {
     console.log(err);
   }
