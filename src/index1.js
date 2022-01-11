afc6bc4a4c869cc73c0d17b9b935d7122826b24d
AndyTiTi <458666482@qq.com>
2022-01-11 14:31:17 +0800

    first c

---
+++ src/index1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <div>
+    <img src="https://i0.hdslb.com/bfs/face/bf5a0cef23c3954f984929e6e9f014b64374a359.jpg@96w_96h_1c_1s.webp" alt="" />
+    <button>Clicke Me</button>
+  </div>
+)
